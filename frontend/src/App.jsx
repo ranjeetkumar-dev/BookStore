67bee165392bfbc7f95becff43cc3b35b0485971
ranjeet <ranjeetkumargond000@gmail.com>
2024-07-13 18:14:50 +0530

    backend completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Home from './pages/Home'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Footer />
+
+
+
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Navbar() {
+  const links = [
+    { title: "Home", link: "/" },
+    { title: "About Us", link: "/about-us" },
+    { title: "All Books", link: "/all-books" },
+    { title: "Cart", link: "/cart" },
+    { title: "Profile", link: "/profile" },
+  ];
+  return (
+    <div className="  bg-zinc-800 text-white px-4 py-4 flex items-center justify-between">
+      <div className=" flex items-center">
+        <img
+          className=" h-10 me-4"
+          src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png"
+          alt="logo"
+        />
+        <h1 className=" text-2xl font-semibold">Book Store</h1>
+
+      </div>
+
+      <div className="nav-links-bookHeaven flex items-center gap-4">
+        <div className="flex gap-4">
+
+          {links.map((items, i) => (
+            <div
+              className=" hover:text-blue-500 transition-all duration-300"
+              key={i}
+            >
+              {items.title}
+
+            </div>
+          ))}
+        </div>
+        <div className=" flex gap-4">
+
+          <button className=" px-2 py-1 border border-blue-500 rounded  hover:bg-white hover:text-zinc-800  transition-all duration-300">
+            Login
+          </button>
+          
+          <button className=" px-2 py-1 border border-blue-500 rounded  hover:bg-white hover:text-zinc-800  transition-all duration-300">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
